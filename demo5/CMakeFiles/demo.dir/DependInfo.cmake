84227a0f486832be4a12a395b7a30a164b08580c
xuek <xuenke@gs-robot.com>
2019-07-15 15:54:13 +0800

    feat: add demo5 for 'set' command

---
+++ demo5/CMakeFiles/demo.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xuek/dev/cmake_demo/demo5/main.cpp" "/home/xuek/dev/cmake_demo/demo5/CMakeFiles/demo.dir/main.cpp.o"
+  "/home/xuek/dev/cmake_demo/demo5/math/math_functions.cpp" "/home/xuek/dev/cmake_demo/demo5/CMakeFiles/demo.dir/math/math_functions.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "math"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
